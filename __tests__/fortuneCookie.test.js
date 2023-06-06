5a9b01f0e5974cdd62b1e6e8e9a19adc588437af
bsalvania <bsalvania@ucsd.edu>
2023-06-05 17:24:35 -0700

    Renamed javascript files

---
+++ __tests__/fortuneCookie.test.js
@@ -0,0 +1,5 @@
+// const getRandomFortune = require('../source/FortuneCookie/fortuneCookie.js');
+// test('adds 1 + 2 to equal 3', () => {
+//     const fortune = getRandomFortune();
+//     expect(getRandomFortune()).toBe(fortune);
+//   });
