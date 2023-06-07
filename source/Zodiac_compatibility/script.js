47b44a94f1f7fb972880f55b9f6f8613fedf31ba
ohuynh21 <100678197+ohuynh21@users.noreply.github.com>
2023-06-06 22:38:38 -0700

    fixed all bugs caused by UI update
    all of the bugs that were caused by the UI update have been resolved, both wheels now spin and their mapping has been updated to account for their new design. font sizes have also been adjusted to be dynamic

--- source/Zodiac_compatibility/script.js
+++ source/Zodiac_compatibility/script.js
@@ -1,12 +1,10 @@
 // Get the wheel element
 //const wheel = document.querySelector('.wheel');
-var wheel1 = document.getElementById("wheel1");
-var wheel2 = document.getElementById("wheel2");
+var wheel1 = document.getElementById("left_wheel_img");
+var wheel2 = document.getElementById("right_wheel_img");
 // Set initial rotation angle
 let w1angle = 0;
-let w1anglereal = 0;
 let w2angle = 0;
-let w2anglereal = 0;
 
 // Function to handle the mouse wheel event
 function rotateWheel1(event) {
@@ -14,11 +12,10 @@ function rotateWheel1(event) {
   const direction = Math.sign(event.deltaY);
 
   // Update the rotation angle based on the scrolling direction
-  w1angle += direction * 2;
-  w1anglereal = w1angle % 360;
-  console.log(w1anglereal);
+  w1angle += direction * 5;
+  console.log(w1angle);
   // Apply the rotation transform to the wheel element
-  wheel1.style.transform = `rotate(${w1anglereal}deg)`;
+  wheel1.style.transform = `rotate(${w1angle}deg)`;
 
   // Prevent the default scrolling behavior
   event.preventDefault();
@@ -29,10 +26,9 @@ function rotateWheel2(event) {
   const direction = Math.sign(event.deltaY);
 
   // Update the rotation angle based on the scrolling direction
-  w2angle += direction * 2;
-  w2anglereal = w2angle % 360;
+  w2angle += direction * 5;
   // Apply the rotation transform to the wheel element
-  wheel2.style.transform = `rotate(${w2anglereal}deg)`;
+  wheel2.style.transform = `rotate(${w2angle}deg)`;
 
   // Prevent the default scrolling behavior
   event.preventDefault();
@@ -42,37 +38,45 @@ function rotateWheel2(event) {
 //wheel.addEventListener('wheel', rotateWheel);
 // Function to round the angle to the nearest multiple of 30
 function roundAngle(angle) {
-  return Math.round(angle / 30) * 30;
+  var base = Math.floor(angle / 360);
+  var rem = angle % 360;
+  if (angle >= 0) {
+    return base * 360 + Math.round(rem / 30) * 30;
+  } else {
+    base = Math.ceil(angle / 360); // Use Math.ceil instead of Math.floor
+    //console.log(base + ';' + rem)
+    return base * 360 + Math.round(rem / 30) * 30;
+  }
 }
 
 // Function to handle the mouseout event
 function stopRotation() {
   // Round the current angle of the wheels to the nearest multiple of 30
-  const target1 = roundAngle(w1anglereal);
-  const target2 = roundAngle(w2anglereal);
+  const target1 = roundAngle(w1angle);
+  const target2 = roundAngle(w2angle);
   // print rounded angles for clarity
   console.log(`wheel 1 is rounded to ${target1}`);
   console.log(`wheel 2 is rounded to ${target2}`);
 
   // Apply the rounded rotation transform to the wheel elements smoothly over 500ms
   const interval = setInterval(() => {
-    if (w1anglereal < target1) {
-      w1anglereal += 1;
-      wheel1.style.transform = `rotate(${w1anglereal}deg)`;
+    if (w1angle< target1) {
+      w1angle += 1;
+      wheel1.style.transform = `rotate(${w1angle}deg)`;
     }
-    if (w1anglereal > target1) {
-      w1anglereal -= 1;
-      wheel1.style.transform = `rotate(${w1anglereal}deg)`;
+    if (w1angle > target1) {
+      w1angle -= 1;
+      wheel1.style.transform = `rotate(${w1angle}deg)`;
     }
-    if (w2anglereal < target2) {
-      w2anglereal += 1;
-      wheel2.style.transform = `rotate(${w2anglereal}deg)`;
+    if (w2angle < target2) {
+      w2angle += 1;
+      wheel2.style.transform = `rotate(${w2angle}deg)`;
     }
-    if (w2anglereal > target2) {
-      w2anglereal -= 1;
-      wheel2.style.transform = `rotate(${w2anglereal}deg)`;
+    if (w2angle > target2) {
+      w2angle -= 1;
+      wheel2.style.transform = `rotate(${w2angle}deg)`;
     }
-    if (w1anglereal === target1 && w2anglereal === target2) {
+    if (w1angle === target1 && w2angle === target2) {
       clearInterval(interval);
     }
   }, 15);
@@ -86,96 +90,106 @@ wheel2.addEventListener("mouseout", stopRotation);
 
 var button = document.getElementById("find-out");
 const popup = document.getElementById("pop-up");
-
-console.log(popup);
+const left_arrow = document.getElementById("left_arrow");
+const right_arrow = document.getElementById("right_arrow");
+const left_bday = document.getElementById("left_bday_input");
+const right_bday = document.getElementById("right_bday_input");
+var relationship_selector = document.querySelector(".relationship");
 button.addEventListener("click", () => {
-  const pair = determinePairing(w1anglereal, w2anglereal);
+  stopRotation()
+  const pair = determinePairing(w1angle, w2angle);
   //console.log(pair);
   wheel1.style.animation = "slideOffLeft 1s forwards";
   wheel2.style.animation = "slideOffRight 1s forwards";
-  button.style.animation = "fadeOut 1s forwards";
+  left_arrow.style.animation = "slideOffLeft 0.1s forwards";
+  right_arrow.style.animation = "slideOffRight 0.1s forwards";
+  left_bday.style.animation = "slideOffLeft 0.4s forwards";
+  right_bday.style.animation = "slideOffRight 0.4s forwards";
+  relationship_selector.style.animation = "fadeOut 0.5s forwards";
+  button.style.animation = "fadeOut 0.5s forwards";
+  
   setTimeout(() => {
     popup.style.display = "block";
-    popup.style.animation = "fadeIn 3s forwards";
+    popup.style.animation = "fadeIn 4s forwards";
     const pairingHeader = popup.querySelector("#pairing");
     pairingHeader.textContent = pair;
   }, 2);
 });
 
 const mapping1 = [
-  [0, "aquarius"],
-  [360, "aquarius"],
+  [0, "Capricorn"],
+  [360, "Capricorn"],
 
-  [30, "pisces"],
-  [-330, "pisces"],
+  [30, "Sagittarius"],
+  [-330, "Sagittarius"],
 
-  [60, "aries"],
-  [-300, "aries"],
+  [60, "Scorpio"],
+  [-300, "Scorpio"],
 
-  [90, "taurus"],
-  [-270, "taurus"],
+  [90, "Libra"],
+  [-270, "Libra"],
 
-  [120, "gemini"],
-  [-240, "gemini"],
+  [120, "Virgo"],
+  [-240, "Virgo"],
 
-  [150, "cancer"],
-  [-210, "cancer"],
+  [150, "Leo"],
+  [-210, "Leo"],
 
-  [180, "leo"],
-  [-180, "leo"],
+  [180, "Cancer"],
+  [-180, "Cancer"],
 
-  [210, "virgo"],
-  [-150, "virgo"],
+  [210, "Gemini"],
+  [-150, "Gemini"],
 
-  [240, "libra"],
-  [-120, "libra"],
+  [240, "Taurus"],
+  [-120, "Taurus"],
 
-  [270, "scorpio"],
-  [-90, "scorpio"],
+  [270, "Aries"],
+  [-90, "Aries"],
 
-  [300, "sagittarius"],
-  [-60, "scorpio"],
+  [300, "Pisces"],
+  [-60, "Pisces"],
 
-  [330, "capricorn"],
-  [-30, "scorpio"],
+  [330, "Aquarius"],
+  [-30, "Aquarius"],
 ];
 
 const mapping2 = [
-  [0, "leo"],
-  [360, "leo"],
+  [0, "Cancer"],
+  [360, "Cancer"],
 
-  [30, "virgo"],
-  [-330, "virgo"],
+  [30, "Gemini"],
+  [-330, "Gemini"],
 
-  [60, "libra"],
-  [-300, "libra"],
+  [60, "Taurus"],
+  [-300, "Taurus"],
 
-  [90, "scorpio"],
-  [-270, "scorpio"],
+  [90, "Aries"],
+  [-270, "Aries"],
 
-  [120, "sagittarius"],
-  [-240, "sagittarius"],
+  [120, "Pisces"],
+  [-240, "Pisces"],
 
-  [150, "capricorn"],
-  [-210, "capricorn"],
+  [150, "Aquarius"],
+  [-210, "Aquarius"],
 
-  [180, "aquarius"],
-  [-180, "aquarius"],
+  [180, "Capricorn"],
+  [-180, "Capricorn"],
 
-  [210, "pisces"],
-  [-150, "pisces"],
+  [210, "Sagittarius"],
+  [-150, "Sagittarius"],
 
-  [240, "aires"],
-  [-120, "aires"],
+  [240, "Scorpio"],
+  [-120, "Scorpio"],
 
-  [270, "taurus"],
-  [-90, "taurus"],
+  [270, "Libra"],
+  [-90, "Libra"],
 
-  [300, "gemini"],
-  [-60, "gemini"],
+  [300, "Virgo"],
+  [-60, "Virgo"],
 
-  [330, "cancer"],
-  [-30, "cancer"],
+  [330, "Leo"],
+  [-30, "Leo"],
 ];
 
 function getMapping1(angle1) {
@@ -196,7 +210,22 @@ function getMapping2(angle2) {
 }
 
 function determinePairing(angle1, angle2) {
+  angle1 = angle1%360
+  angle2 = angle2%360
   const angle1Mapping = getMapping1(angle1);
   const angle2Mapping = getMapping2(angle2);
-  return `wheel1's mapping is ${angle1Mapping} and wheel2's mapping is ${angle2Mapping}.`;
+  return `${angle1Mapping} and ${angle2Mapping}`;
+}
+
+function determineResults(pairing, relationship) {
+  if (relationship === romantic){
+
+  }
+  else if (relationship === professional){
+
+  }
+  else{
+    
+  }
+
 }
