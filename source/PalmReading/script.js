6c65321823dde2b161dc33849eea4efcc1b68c3f
Ayaaa99 <chz011@ucsd.edu>
2023-06-08 20:38:35 -0700

    added contents for other hand lines in palm reading section

--- source/PalmReading/script.js
+++ source/PalmReading/script.js
@@ -7,3 +7,17 @@ const fortune = document.getElementById("fortune-paragraph");
 export function handleFortune() {
   console.log("TODO", fortune);
 }
+
+const tabLabels = document.querySelectorAll('input[name="tab"]');
+const tabContents = document.querySelectorAll('.tab-content');
+
+tabLabels.forEach(function(label){
+  label.addEventListener('change',function(){
+    tabContents.forEach(function(content){
+      content.hidden = true;
+    });
+
+    const selectedContent = document.getElementById(`${this.value}-tab`);
+    selectedContent.hidden = false;
+  });
+});
