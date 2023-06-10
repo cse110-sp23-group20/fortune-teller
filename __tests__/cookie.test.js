6c03637f14d6d7604ec9a908c7db47ab847e3396
bsalvania <bsalvania@ucsd.edu>
2023-06-10 14:51:35 -0700

    Added test for cookie button, may need fix

--- __tests__/cookie.test.js
+++ __tests__/cookie.test.js
@@ -44,6 +44,41 @@ describe("Basic user flow for Website", () => {
     expect(fortuneButtonDisabled).toBe(true);
   }, 5000);
 
+  // Check to make sure that cookie button also disables when the fortune button is clicked
+  it("Make sure cookie button disables after the fortune button is clicked", async () => {
+    console.log(
+      "Checking to make sure fortune button disables when fortune button is clicked..."
+    );
+
+    let cookieButtonDisabled;
+    const cookieButton = await page.$("#cookie-button");
+
+    let booleanValue = await cookieButton.getProperty("disabled");
+    cookieButtonDisabled = await booleanValue.jsonValue();
+    expect(cookieButtonDisabled).toBe(true);
+  }, 5000);
+
+  // Check to make sure that reset button is enabled when after fortune button is clicked
+  it("Make sure reset button is enabled after fortune button is clicked", async () => {
+    console.log("Checking to make sure reset button is enabled...");
+
+    /**
+     * The waitForFunction() waits for the provided function to return true, indicating that the button element with the ID 'reset-button'
+     * has its disabled attribute set to false. If the button's disabled value becomes false within the specified timeout, the test will pass.
+     */
+    await page.waitForFunction(() => {
+      const resetButton = document.querySelector("#reset-button");
+      return resetButton && resetButton.disabled === false;
+    });
+
+    // Makes sure the reset button is actually enabled
+    let resetButtonDisabled;
+    const resetButton = await page.$("#reset-button");
+    let booleanValue = await resetButton.getProperty("disabled");
+    resetButtonDisabled = await booleanValue.jsonValue();
+    expect(resetButtonDisabled).toBe(false);
+  }, 25000);
+
   // Check to make sure that reset button is enabled when after fortune button is clicked
   it("Make sure reset button is enabled after fortune button is clicked", async () => {
     console.log("Checking to make sure reset button is enabled...");
