ff779c8c5fb52c0aaa89411c15f345f70d79d6ab
bsalvania <bsalvania@ucsd.edu>
2023-06-10 14:31:06 -0700

    Modified and made E2E tests for cookie

--- __tests__/cookie.test.js
+++ __tests__/cookie.test.js
@@ -6,64 +6,84 @@ describe("Basic user flow for Website", () => {
     );
   });
 
-  // Check to make sure that button is enabled when page loads
-  it("Make sure button is enabled", async () => {
-    console.log("Checking to make sure button is enabled...");
-    let buttonDisabled;
-    const button = await page.$("#fortune-button");
-    let booleanValue = await button.getProperty("disabled");
-    buttonDisabled = await booleanValue.jsonValue();
-    expect(buttonDisabled).toBe(false);
+  // Check to make sure that fortune button is enabled when page loads
+  it("Make sure fortune button is enabled", async () => {
+    console.log("Checking to make sure fortune button is enabled...");
+    let fortuneButtonDisabled;
+    const fortuneButton = await page.$("#fortune-button");
+
+    let booleanValue = await fortuneButton.getProperty("disabled");
+    fortuneButtonDisabled = await booleanValue.jsonValue();
+    expect(fortuneButtonDisabled).toBe(false);
   }, 5000);
 
-  // Check to make sure that button disables when it is clicked
-  it("Make sure button disables", async () => {
-    console.log("Checking to make sure button disables when clicked...");
-    let buttonDisabled;
-    const button = await page.$("#fortune-button");
-    await button.click();
-    let booleanValue = await button.getProperty("disabled");
-    buttonDisabled = await booleanValue.jsonValue();
-    expect(buttonDisabled).toBe(true);
+  // Check to make sure that reset button is disabled when page loads
+  it("Make sure reset button is disabled", async () => {
+    console.log("Checking to make sure reset button is disabled...");
+    let resetButtonDisabled;
+    const resetButton = await page.$("#reset-button");
+
+    let booleanValue = await resetButton.getProperty("disabled");
+    resetButtonDisabled = await booleanValue.jsonValue();
+    console.log("How is this true: " + booleanValue);
+    expect(resetButtonDisabled).toBe(true);
   }, 5000);
 
-  // Check to make sure that button reenables after it is clicked
-  //   it('Make sure button is reenabled', async () => {
-  //     console.log('Checking to make sure button reenables after being clicked...');
-  //     // Wait because it takes time until button is reenabled as it has to read out the fortune
-  //     await page.waitForTimeout(15000);
-  //     let buttonDisabled;
-  //     const button = await page.$('button');
-  //     let booleanValue = await button.getProperty('disabled');
-  //     buttonDisabled = await booleanValue.jsonValue();
-  //     expect(buttonDisabled).toBe(false);
-  // }, 20000)
+  // Check to make sure that fortune button disables when it is clicked
+  it("Make sure fortune button disables after being clicked", async () => {
+    console.log(
+      "Checking to make sure fortune button disables when clicked..."
+    );
 
-  // Check to make sure audio disables if user unchecks 'toggle voice' checkbox
-  //   it('Make sure audio disables', async () => {
-  //     console.log('Checking to make sure audio disables after \'toggle voice\' is unchecked...');
-  //     // Wait because it takes time until button is reenabled as it has to read out the fortune
-  // }, 15000)
+    let fortuneButtonDisabled;
+    const fortuneButton = await page.$("#fortune-button");
+    await fortuneButton.click();
+
+    let booleanValue = await fortuneButton.getProperty("disabled");
+    fortuneButtonDisabled = await booleanValue.jsonValue();
+    expect(fortuneButtonDisabled).toBe(true);
+  }, 5000);
+
+  // Check to make sure that reset button is enabled when after fortune button is clicked
+  it("Make sure reset button is enabled after fortune button is clicked", async () => {
+    console.log("Checking to make sure reset button is enabled...");
+
+    /**
+     * The waitForFunction() waits for the provided function to return true, indicating that the button element with the ID 'reset-button'
+     * has its disabled attribute set to false. If the button's disabled value becomes false within the specified timeout, the test will pass.
+     */
+    await page.waitForFunction(() => {
+      const resetButton = document.querySelector("#reset-button");
+      return resetButton && resetButton.disabled === false;
+    });
+
+    // Makes sure the reset button is actually enabled
+    let resetButtonDisabled;
+    const resetButton = await page.$("#reset-button");
+    let booleanValue = await resetButton.getProperty("disabled");
+    resetButtonDisabled = await booleanValue.jsonValue();
+    expect(resetButtonDisabled).toBe(false);
+  }, 25000);
 
   // Check to make sure that the speech synthesis is being populated, the correct voices are called by the function
-  describe("populateVoice", () => {
-    test("populate voice options", () => {
-      const voices = [
-        { name: "Karen", lang: "en-AU", default: false },
-        { name: "Eddy", lang: "pt-BR", default: false },
-        { name: "Lesya", lang: "uk-UA", default: false },
-      ];
-      const synthesis = {
-        getVoices: jest.fn().mockReturnValue(voices),
-      };
-      //const option = document.createElement('option');
-      const appendchild = jest.spyOn(voiceSelect, "appendChild");
-      //document.body.innerHTML=
-      //<select></select>;
-      populateVoiceList();
-      expect(synthesis.getVoices).toHaveBeenCalled();
-      expect(appendchild).toHaveBeenCalledTimes(3);
-      //expect(voiceSelect.innerHTML).toContain('Karen');
-    });
-  });
+  // describe("populateVoice", () => {
+  //   test("populate voice options", () => {
+  //     const voices = [
+  //       { name: "Karen", lang: "en-AU", default: false },
+  //       { name: "Eddy", lang: "pt-BR", default: false },
+  //       { name: "Lesya", lang: "uk-UA", default: false },
+  //     ];
+  //     const synthesis = {
+  //       getVoices: jest.fn().mockReturnValue(voices),
+  //     };
+  //     //const option = document.createElement('option');
+  //     const appendchild = jest.spyOn(voiceSelect, "appendChild");
+  //     //document.body.innerHTML=
+  //     //<select></select>;
+  //     populateVoiceList();
+  //     expect(synthesis.getVoices).toHaveBeenCalled();
+  //     expect(appendchild).toHaveBeenCalledTimes(3);
+  //     //expect(voiceSelect.innerHTML).toContain('Karen');
+  //   });
+  // });
 });
