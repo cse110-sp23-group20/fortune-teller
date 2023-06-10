e97c5d0cfdc224b61af4b3b438adf018b66e64be
ohuynh21 <100678197+ohuynh21@users.noreply.github.com>
2023-06-10 16:17:28 -0700

    made the buttons unclickable before they appear

--- source/Zodiac_compatibility/script.js
+++ source/Zodiac_compatibility/script.js
@@ -172,5 +172,12 @@ document.addEventListener("DOMContentLoaded", function () {
   left_arrow.style.animation = "appear 1s 2s forwards";
   right_arrow.style.animation = "appear 1s 2s forwards";
   button.style.animation = "appear 1s 2.5s forwards";
+  setTimeout(() => {
+    button.style.pointerEvents = 'auto';
+  }, 3500);
   how_to.style.animation = "appear 1s 3.25s forwards";
+  setTimeout(() => {
+    how_to.style.pointerEvents = 'auto';
+  }, 4250);
+
 });
