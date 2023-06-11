cb82bbff1141284ee1397dc9d19b7485ff46ca8b
ohuynh21 <100678197+ohuynh21@users.noreply.github.com>
2023-06-11 14:00:45 -0700

    try Array<Array<number, string>>

--- source/Zodiac_compatibility/data/dataArray.js
+++ source/Zodiac_compatibility/data/dataArray.js
@@ -1,6 +1,6 @@
 /**
  * A mapping of modulo'd angles to Zodiacs for the left wheel.
- * @type {[[number, string]]}
+ * @type {Array<Array<number, string>>}
  */
 const zodiacAngleMappingLeft = [
   [0, "Capricorn"],
@@ -41,7 +41,7 @@ const zodiacAngleMappingLeft = [
 ];
 /**
  * A mapping of modulo'd angles to Zodiacs for the right wheel.
- * @type {[[number, string]]}
+ * @type {Array<Array<number, string>>}
  */
 const zodiacAngleMappingRight = [
   [0, "Cancer"],
